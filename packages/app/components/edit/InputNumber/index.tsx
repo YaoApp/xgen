3d5e7974ea60d473d621ba25a5f6c8cda624a9fb
Wendao <xiewendao123@foxmail.com>
2022-04-26 12:46:40 +0800

    [migrate] InputNumber

---
+++ packages/app/components/edit/InputNumber/index.tsx
@@ -0,0 +1,25 @@
+import { InputNumber } from 'antd'
+
+import Item from '@/components/edit/Item'
+import { getLocale } from '@umijs/max'
+
+import type { InputNumberProps } from 'antd'
+import type { Component } from '@/types'
+
+interface IProps extends InputNumberProps, Component.PropsEditComponent {}
+
+const Index = (props: IProps) => {
+	const { __bind, __name, itemProps, ...rest_props } = props
+	const is_cn = getLocale() === 'zh-CN'
+
+	return (
+		<Item {...itemProps} {...{ __bind, __name }}>
+			<InputNumber
+				placeholder={`${is_cn ? '请输入' : 'Please input '}${__name}`}
+				{...rest_props}
+			></InputNumber>
+		</Item>
+	)
+}
+
+export default window.$app.memo(Index)
