c1fea6d49e6d141364d118fc1fbfca49c72da2ea
Wendao <xiewendao123@foxmail.com>
2022-04-27 15:04:01 +0800

    [migrate] TextArea

---
+++ packages/app/components/edit/TextArea/index.tsx
@@ -0,0 +1,27 @@
+import { Input } from 'antd'
+
+import Item from '@/components/edit/Item'
+import { getLocale } from '@umijs/max'
+
+import type { TextAreaProps } from 'antd/lib/input/TextArea'
+import type { Component } from '@/types'
+
+const { TextArea } = Input
+
+interface IProps extends TextAreaProps, Component.PropsEditComponent {}
+
+const Index = (props: IProps) => {
+	const { __bind, __name, itemProps, ...rest_props } = props
+	const is_cn = getLocale() === 'zh-CN'
+
+	return (
+		<Item {...itemProps} {...{ __bind, __name }}>
+			<TextArea
+				placeholder={`${is_cn ? '请输入' : 'Please input '}${__name}`}
+				{...rest_props}
+			></TextArea>
+		</Item>
+	)
+}
+
+export default window.$app.memo(Index)
