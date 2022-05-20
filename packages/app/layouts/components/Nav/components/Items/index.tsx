3161af13a0c53f22bfb1dd087de7ec68ea2001d0
Wendao <xiewendao123@foxmail.com>
2022-05-21 05:18:11 +0800

    [add] overflow handler for nav bar

--- packages/app/layouts/components/Nav/components/Items/index.tsx
+++ packages/app/layouts/components/Nav/components/Items/index.tsx
@@ -10,20 +10,24 @@ import type { IPropsItems } from '@/layouts/types'
 
 const Index = ({ menu, current_nav }: IPropsItems) => {
 	return (
-		<div className={styles._local}>
-			{menu.map((item, index) => (
-				<Tooltip title={item.name} placement='right' key={index}>
-					<Link
-						className={clsx([
-							'nav_item w_100 flex justify_center align_center clickable',
-							current_nav === index ? 'active' : ''
-						])}
-						to={item.path}
-					>
-						<Icon name={item.icon} size={20}></Icon>
-					</Link>
-				</Tooltip>
-			))}
+		<div className={clsx([styles._local])}>
+			<div className='top_mask w_100 mask absolute'></div>
+			<div className='bottom_mask w_100 mask absolute'></div>
+			<div className='w_100 border_box flex flex_column'>
+				{menu.map((item, index) => (
+					<Tooltip title={item.name} placement='right' key={index}>
+						<Link
+							className={clsx([
+								'nav_item w_100 flex justify_center align_center clickable',
+								current_nav === index ? 'active' : ''
+							])}
+							to={item.path}
+						>
+							<Icon name={item.icon} size={20}></Icon>
+						</Link>
+					</Tooltip>
+				))}
+			</div>
 		</div>
 	)
 }
