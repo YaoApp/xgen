0719bd3bb663f00b8e8427fca50b8fb65013581f
MatrixAge <xiewendao123@foxmail.com>
2022-09-28 00:39:32 +0800

    [migrate] migrate types to studio for dev

--- packages/types/.fatherrc.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'father'
-import { resolve } from 'path'
-
-export default defineConfig({
-	esm: { transformer: 'esbuild' },
-	alias: { '@': resolve(__dirname, './src') }
-})
--- packages/types/src/index.ts
+++
@@ -1,5 +0,0 @@
-import { App as _App } from './app'
-
-export namespace Yao {
-	export type App = _App
-}
