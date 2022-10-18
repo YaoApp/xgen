17a4cb541a7efc574940827c36ae7c3a67c219c2
Wendao <xiewendao123@foxmail.com>
2022-10-19 01:05:22 +0800

    [setup:add] build as html

--- packages/setup/svelte.config.js
+++ packages/setup/svelte.config.js
@@ -1,5 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
-import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-static'
+import preprocess from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -10,6 +10,6 @@ const config = {
 	kit: {
 		adapter: adapter()
 	}
-};
+}
 
-export default config;
+export default config
