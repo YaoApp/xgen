180bb37dff911be9e57913ac3a614bdc3f47f469
MatrixAge <xiewendao123@foxmail.com>
2022-10-28 01:11:23 +0800

    [fix] Image outer src

--- packages/xgen/knife/yao/getFileSrc.ts
+++ packages/xgen/knife/yao/getFileSrc.ts
@@ -1,6 +1,8 @@
 import getToken from './getToken'
 
 const Index = (name: string) => {
+	if (name.startsWith('http')) return name
+
 	return `/api/${window.$app.api_prefix}/storage/url?name=${name}&token=${getToken()}`
 }
 
