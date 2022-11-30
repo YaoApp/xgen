ccc9cad30e95e4203b18ab459bd9d9fcc3b1fff6
Wendao <xiewendao123@foxmail.com>
2022-12-01 04:05:48 +0800

    [fix] render error by third level menu

--- packages/xgen/layouts/components/Menu/hooks/useMenuItems.tsx
+++ packages/xgen/layouts/components/Menu/hooks/useMenuItems.tsx
@@ -1,46 +1,42 @@
-import { Fragment, useMemo } from 'react'
+import { Fragment } from 'react'
 
 import type { IPropsMenu } from '../../../types'
 
 const Index = (items: IPropsMenu['items'], prefix?: string) => {
-	return useMemo(() => {
-		return items.reduce(
-			(total: { menu_items: Array<any>; pure_items: Array<any> }, item) => {
-				const menu_item: any = {}
-				const pure_item: any = {}
-				const _prefix = `${prefix ?? ''}/${item.name}`
-				const key = `${_prefix}|${item.path}`
+	return items.reduce(
+		(total: { menu_items: Array<any>; pure_items: Array<any> }, item) => {
+			const menu_item: any = {}
+			const pure_item: any = {}
+			const _prefix = `${prefix ?? ''}/${item.name}`
+			const key = `${_prefix}|${item.path}`
 
-				if (item.badge) {
-					menu_item['label'] = (
-						<Fragment>
-							{item.name}
-							<span className='badge_wrap'>
-								{item.badge}
-							</span>
-						</Fragment>
-					)
-				} else {
-					menu_item['label'] = item.name
-				}
-				menu_item['key'] = key
+			if (item.badge) {
+				menu_item['label'] = (
+					<Fragment>
+						{item.name}
+						<span className='badge_wrap'>{item.badge}</span>
+					</Fragment>
+				)
+			} else {
+				menu_item['label'] = item.name
+			}
+			menu_item['key'] = key
 
-				pure_item['label'] = item.name
-				pure_item['key'] = key
+			pure_item['label'] = item.name
+			pure_item['key'] = key
 
-				if (item?.children?.length) {
-					menu_item['children'] = Index(item.children, _prefix).menu_items
-					pure_item['children'] = Index(item.children, _prefix).pure_items
-				}
+			if (item?.children && item?.children?.length) {
+				menu_item['children'] = Index(item.children, _prefix).menu_items
+				pure_item['children'] = Index(item.children, _prefix).pure_items
+			}
 
-				total.menu_items.push(menu_item)
-				total.pure_items.push(pure_item)
+			total.menu_items.push(menu_item)
+			total.pure_items.push(pure_item)
 
-				return total
-			},
-			{ menu_items: [], pure_items: [] }
-		)
-	}, [items, prefix])
+			return total
+		},
+		{ menu_items: [], pure_items: [] }
+	)
 }
 
 export default Index
