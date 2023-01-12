996113b541cf8116120efa625b4f72dcd30abb62
Wendao <xiewendao123@foxmail.com>
2023-01-12 17:46:21 +0800

    [test] getPath

--- packages/xgen/utils/filter/getPath.ts
+++ packages/xgen/utils/filter/getPath.ts
@@ -1,6 +1,7 @@
 export default (pathname: string) => {
 	console.log('pathname: ', pathname)
 	console.log('$runtime.BASE: ', $runtime.BASE)
+	console.log('/$runtime.BASE: ', `/${$runtime.BASE}`)
 
 	console.log('res: ', pathname.replace(`/${$runtime.BASE}`, ''))
 
