fff181de2fccd4081515362c376cb1160612da3c
Wendao <xiewendao123@foxmail.com>
2023-02-10 16:14:05 +0800

    [add] Text: support object value like { value, color }

--- packages/xgen/components/view/Text/index.tsx
+++ packages/xgen/components/view/Text/index.tsx
@@ -1,34 +1,50 @@
+import { isPlainObject } from 'lodash-es'
 import moment from 'moment'
+import { useMemo } from 'react'
 
 import type { Component } from '@/types'
 
+type Value = string | number | Array<string | number>
+type StyledValue = { value: Value; color: string }
+
 interface IProps extends Component.PropsViewComponent {
+	__value: Value | StyledValue
 	format?: string
-	color?: string
 }
 
 const Index = (props: IProps) => {
-	const { __value, color } = props
+	const { __value } = props
 
 	if (!__value) return <span>-</span>
 
-	const style = color ? { color } : undefined
+	const { value, style } = useMemo(() => {
+		if (isPlainObject(__value)) {
+			const styled_value = __value as StyledValue
+
+			return {
+				value: styled_value?.value,
+				style: styled_value?.color ? { color: styled_value.color } : undefined
+			}
+		} else {
+			return { value: __value as Value }
+		}
+	}, [__value])
 
 	if (props?.format) {
-		if (Array.isArray(__value)) {
+		if (Array.isArray(value)) {
 			return (
 				<span style={style}>
-					<span>{moment(__value[0]).format(props.format)}</span>
+					<span>{moment(value[0]).format(props.format)}</span>
 					<span className='ml_6 mr_6'>-</span>
-					<span>{moment(__value[1]).format(props.format)}</span>
+					<span>{moment(value[1]).format(props.format)}</span>
 				</span>
 			)
 		}
 
-		return <span style={style}>{moment(__value).format(props.format)}</span>
+		return <span style={style}>{moment(value).format(props.format)}</span>
 	}
 
-	return <span style={style}>{__value}</span>
+	return <span style={style}>{value}</span>
 }
 
 export default window.$app.memo(Index)
