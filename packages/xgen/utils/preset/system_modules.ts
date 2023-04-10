2b3c7d43503a19a4613a23ad22b90282218c201a
Wendao <xiewendao123@foxmail.com>
2023-04-10 23:34:01 +0800

    [add] runtime module: ahooks & optimization for system modules

--- packages/xgen/utils/preset/system_modules.ts
+++ packages/xgen/utils/preset/system_modules.ts
@@ -1,46 +1,77 @@
 import 'systemjs/dist/extras/amd'
 import 'systemjs/dist/extras/use-default'
 
+import {
+	useAsyncEffect,
+	useClickAway,
+	useDeepCompareEffect,
+	useFullscreen,
+	useKeyPress,
+	useMemoizedFn,
+	useMount,
+	useSize,
+	useToggle
+} from 'ahooks'
 import { Form, Input } from 'antd'
 import { deepEqual } from 'fast-equals'
-import * as Mobx from 'mobx'
-import * as MobxReactLite from 'mobx-react-lite'
+import { autorun, configure, makeAutoObservable, reaction, toJS } from 'mobx'
+import { observer } from 'mobx-react-lite'
 import * as React from 'react'
 import * as ReactDom from 'react-dom'
 import * as ReactDomClient from 'react-dom/client'
-import JsxRuntime from 'react/jsx-runtime'
+import * as JsxRuntime from 'react/jsx-runtime'
 import { createMakeAndWithStyles } from 'tss-react'
-import * as Tsyringe from 'tsyringe'
+import { container, injectable, singleton } from 'tsyringe'
 
-import { Graph } from '@antv/x6'
+import { Graph, Markup } from '@antv/x6'
 import { register } from '@antv/x6-react-shape'
 
-System.set('app:tss-react', { createMakeAndWithStyles })
-System.set('app:@antv/x6', { Graph })
-System.set('app:@antv/x6-react-shape', { register })
-System.set('app:antd', { Input, Form })
-System.set('app:fast-equals', { deepEqual })
-System.set('app:tsyringe', { ...Tsyringe })
-System.set('app:mobx', { ...Mobx })
-System.set('app:mobx-react-lite', { ...MobxReactLite })
-System.set('app:react/jsx-runtime', { ...JsxRuntime })
 System.set('app:react', { default: React, ...React })
 System.set('app:react-dom', { default: ReactDom, ...ReactDom })
 System.set('app:react-dom/client', { default: ReactDomClient, ...ReactDomClient })
+System.set('app:react/jsx-runtime', { ...JsxRuntime })
+
+System.set('app:tss-react', { createMakeAndWithStyles })
+System.set('app:fast-equals', { deepEqual })
+
+System.set('app:tsyringe', { container, injectable, singleton })
+System.set('app:mobx', { toJS, makeAutoObservable, reaction, autorun, configure })
+System.set('app:mobx-react-lite', { observer })
+
+System.set('app:@antv/x6', { Graph, Markup })
+System.set('app:@antv/x6-react-shape', { register })
+
+System.set('app:antd', { Input, Form })
+System.set('app:ahooks', {
+	useMemoizedFn,
+	useClickAway,
+	useFullscreen,
+	useToggle,
+	useMount,
+	useDeepCompareEffect,
+	useKeyPress,
+	useAsyncEffect,
+	useSize
+})
 
 System.addImportMap({
 	imports: {
-		'tss-react': 'app:tss-react',
-		'@antv/x6': 'app:@antv/x6',
-		'@antv/x6-react-shape': 'app:@antv/x6-react-shape',
-		antd: 'app:antd',
-		tsyringe: 'app:tsyringe',
-		mobx: 'app:mobx',
-		react: 'app:react',
-		'fast-equals': 'app:fast-equals',
-		'mobx-react-lite': 'app:mobx-react-lite',
-		'react/jsx-runtime': 'app:react/jsx-runtime',
-		'react-dom': 'app:react-dom',
-		'react-dom/client': 'app:react-dom/client'
+		[`react`]: 'app:react',
+		[`react-dom`]: 'app:react-dom',
+		[`react-dom/client`]: 'app:react-dom/client',
+		[`react/jsx-runtime`]: 'app:react/jsx-runtime',
+
+		[`tss-react`]: 'app:tss-react',
+		[`fast-equals`]: 'app:fast-equals',
+
+		[`tsyringe`]: 'app:tsyringe',
+		[`mobx`]: 'app:mobx',
+		[`mobx-react-lite`]: 'app:mobx-react-lite',
+
+		[`@antv/x6`]: 'app:@antv/x6',
+		[`@antv/x6-react-shape`]: 'app:@antv/x6-react-shape',
+
+		[`antd`]: 'app:antd',
+		[`ahooks`]: 'app:ahooks'
 	}
 })
