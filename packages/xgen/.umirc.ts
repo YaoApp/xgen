a81595a6b06c48e8e4d38945ee822b2a5611a653
Wendao <xiewendao123@foxmail.com>
2023-04-12 00:49:21 +0800

    [_]

--- packages/xgen/.umirc.ts
+++ packages/xgen/.umirc.ts
@@ -4,7 +4,7 @@ import { base, chainWebpack, conventionRoutes, links, metas, proxy, srcTranspile
 
 export default defineConfig({
 	mfsu: { esbuild: false, strategy: 'eager', exclude: ['monaco-editor-webpack-plugin'] },
-	monorepoRedirect: { srcDir: ['./'] },
+	monorepoRedirect: { srcDir: ['src'] },
 	srcTranspiler: 'swc',
 	srcTranspilerOptions,
 	jsMinifier: 'swc',
