1aed39527bf7e610a8bdfd666e973a415afe7a89
Max <shadow.iqka@qq.com>
2024-03-17 12:37:22 +0800

    Refactor Image component to use Skeleton loading

--- packages/xgen/components/edit/Upload/components/Image.tsx
+++ packages/xgen/components/edit/Upload/components/Image.tsx
@@ -2,20 +2,39 @@ import { getFileSrc } from '@/knife'
 import { DeleteOutlined } from '@ant-design/icons'
 
 import type { IPropsCustomImage } from '../types'
+import { useEffect, useState } from 'react'
+import { Skeleton } from 'antd'
 
 const Index = (props: IPropsCustomImage) => {
 	const { file, imageSize, remove } = props
+	const [loading, setLoading] = useState<boolean>(true)
+	const [url, setUrl] = useState<string>(file.response || '')
+
+	useEffect(() => {
+		if (file.response) {
+			setUrl(getFileSrc(file.response))
+			setLoading(false)
+		}
+	}, [file.response])
 
 	return (
 		<div className='upload_custom_wrap flex relative'>
 			<div className='icon_delete absolute justify_center align_center transition_normal' onClick={remove}>
 				<DeleteOutlined className='icon'></DeleteOutlined>
 			</div>
-			<img
-				className='image'
-				src={getFileSrc(file.response!)}
-				style={{ borderRadius: 6, objectFit: 'cover', ...imageSize }}
-			></img>
+			<Skeleton
+				loading={loading || url == ''}
+				active
+				paragraph={{
+					width: imageSize?.width || '100%'
+				}}
+			>
+				<img
+					className='image'
+					src={getFileSrc(url)}
+					style={{ borderRadius: 6, objectFit: 'cover', ...imageSize }}
+				></img>
+			</Skeleton>
 		</div>
 	)
 }
