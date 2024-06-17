11bfffe3285821af538d6a4dad162299c518e230
Max <shadow.iqka@qq.com>
2024-06-17 11:36:10 +0800

    refactor: Update en.ts locale file with new translations

---
+++ packages/xgen/locales/en.ts
@@ -0,0 +1,9 @@
+import layout from './source/en/layout'
+import login from './source/en/login'
+import messages from './source/en/messages'
+
+export default {
+	...layout,
+	...login,
+	...messages
+}
