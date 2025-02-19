c25a4be2da28b5fae95e6b69c4f1427e4a784dbc
Max <shadow.iqka@qq.com>
2025-02-19 10:07:29 +0800

    Improve tool message parsing and display in chat component

--- packages/xgen/components/chat/tool/index.tsx
+++ packages/xgen/components/chat/tool/index.tsx
@@ -23,23 +23,49 @@ interface IProps extends Component.PropsChatComponent {
 	text?: string
 	chat_id: string
 	pending?: boolean
+	props?: Record<string, any>
 	children?: React.ReactNode
 }
 
-const Index = (props: IProps) => {
-	const { text, chat_id, pending, children } = props
-	const is_cn = getLocale() === 'zh-CN'
-	let content = ''
+const sizeHumanize = (size: number) => {
+	if (size < 1024) {
+		return `${size} B`
+	}
+	return `${(size / 1024).toFixed(2)} KB`
+}
 
+const Index = (props: IProps) => {
+	const { text: text_, chat_id, pending, children, props: props_ } = props
+	const is_cn = getLocale() === 'zh-CN'
+	const text = props_?.text || text_
+	let content = getTextFromHtml(children) || text || (is_cn ? '工具调用请求' : 'Tool call request')
+	content = content
+		.replace(/%7B/g, '{')
+		.replace(/%7D/g, '}')
+		.replace(/%22/g, '"')
+		.replace(/&quot;/g, '"')
+
+	const size = sizeHumanize(content.length)
 	if (!pending) {
-		content = getTextFromHtml(children) || text || (is_cn ? '工具调用请求' : 'Tool call request')
-		try {
-			content = content.replace(/%7B/g, '{').replace(/%7D/g, '}')
-			const json = JSON.parse(content)
-			if (json.function) {
-				content = is_cn ? `工具调用请求: ${json.function}` : `Tool call request: ${json.function}`
-			}
-		} catch (error) {}
+		// "function":"new_feature"
+		// 使用正则表达式提取 function 的值,
+		const function_match = content.match(/\"function\"\s*:\s*\"(\w+)\"/)
+		if (function_match) {
+			content = is_cn ? `工具调用请求: ${function_match[1]}` : `Tool call request: ${function_match[1]}`
+		} else {
+			content = is_cn ? '工具调用请求' : 'Tool call request'
+		}
+
+		// try {
+
+		// 	const json = JSON.parse(content)
+		// 	if (json.function) {
+		// 		content = is_cn ? `工具调用请求: ${json.function}` : `Tool call request: ${json.function}`
+		// 	}
+		// } catch (error) {
+		// 	console.error(content)
+		// 	content = is_cn ? '工具调用请求' : 'Tool call request'
+		// }
 	}
 
 	return (
@@ -47,7 +73,7 @@ const Index = (props: IProps) => {
 			{pending ? (
 				<Loading
 					chat_id={chat_id}
-					placeholder={is_cn ? '工具调用请求' : 'Tool call request'}
+					placeholder={is_cn ? `工具调用请求 ${size}` : `Tool call request ${size}`}
 					icon='material-slow_motion_video'
 				/>
 			) : (
